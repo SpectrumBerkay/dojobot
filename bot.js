da6ec430c10d064644da1694a940f7b5f081861c
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-29 22:14:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+
+var prefix = ("");
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message', msg => {
+  console.log(`Yeni Mesaj Alındı! Mesaj: ${msg.content} Yazan: ${msg.author.tag}`);
+  if (!msg.content.startsWith(prefix)) {
+	  return;
+  }
+  if (msg.content.toLowerCase() === prefix + 'ping') {
+    msg.reply('Pong! **' + client.ping + '** ms');
+  }
+  if (msg.content.toLowerCase() === prefix + 'sa') {
+    msg.reply('Aleyküm Selam!');
+  }
+  if (msg.content.toLowerCase() === prefix + 'fuckmylifexd') {
+    msg.channel.sendMessage('Hoşgeldin Türkcaaaan');
+  }
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(ayarlar.token);
