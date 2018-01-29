ada49bb7fef4f414875104e16856c622b62b2fb0
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-29 22:33:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const ayarlar = require('./ayarlar.json');
 
-var prefix = ("");
+var prefix = ("!");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
