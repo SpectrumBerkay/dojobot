1813dc9d3a02e445c00baaaf071e27dbbc779f83
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-30 09:31:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,13 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
-var prefix = ("!");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-  }
-  if (msg.content.toLowerCase() === 'ping') {
-    msg.reply('Pong! **' + client.ping + '** ms');
-  }
-  if (msg.content.toLowerCase() === 'sa' {
-    msg.reply('Aleyküm Selam!');
-  }
-  if (msg.content.toLowerCase() === 'fuckmylifexd') {
-    msg.channel.sendMessage('Hoşgeldin Türkcaaaan');
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
   }
 });
 
