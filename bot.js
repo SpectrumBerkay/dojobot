2376f69585c05d83283a7b9b6a5a78a5b7954de1
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-30 13:54:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,41 +1,9 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-const Prefix = "fbebsi"
 
+var bot = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
- if (msg.content.toLowerCase() === 'ping') {
-    msg.reply('Pong! **' + client.ping + '** ms');
-  }
- if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('Aleyküm Selam!')
-  }
-  if (msg.content.toLowerCase() === 'merhaba') {
-    msg.reply('Aleyküm Selam!')
-  }
-  if (msg.content.toLowerCase() === 'fotoğrafım ne') {
-    msg.reply(msg.author.avatarURL)
-  }
-  if (msg.content.toLowerCase() === 'selamün aleyküm') {
-    msg.reply('Aleyküm Selam!')
-  }
-  if (msg.content.toLowerCase() === 'bot') {
-    msg.reply('ping,!bot')
-  }
-  
-  var args = msg.content.substring(prefix.lenght).split(" ");
-  
-  switch (args[0]) {
-    case "info":
-      msg.channel.sendMessage("I'm a super dope bot created my me ")
-      break;
-    default:
-      msg.channel.sendMessage("Invalid command")
-  }
+bot.on("meesage", function(message) {
+ console.log(message.content); 
 });
 
 client.login(process.env.BOT_TOKEN);
