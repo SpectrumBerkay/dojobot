418e913efe5535d60c36b4fae4e69f82a9faacc8
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-30 09:33:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+ if (msg.content.toLowerCase() === 'ping') {
+    msg.reply('Pong! **' + client.ping + '** ms');
   }
 });
 
