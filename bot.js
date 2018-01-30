0f0dbc70e9f1189490841bba75a593ca8be999d1
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-30 09:47:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,8 +15,8 @@ client.on('message', msg => {
   if (msg.content.toLowerCase() === 'merhaba') {
     msg.reply('Aleyküm Selam!')
   }
-  if (msg.content.toLowerCase() === 'fuckmylifexdproh') {
-    msg.channel.sendMessage('https://cdn.discordapp.com/attachments/380685465965035523/390991230148018177/stats.png')
+  if (msg.content.toLowerCase() === 'fotoğrafım ne') {
+    message.reply(message.author.avatarURL)
   }
 });
 
