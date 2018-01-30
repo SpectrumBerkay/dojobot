7815731d15b999e4234e6e8500dbef0f218d0d55
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-30 13:55:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,31 @@
 const Discord = require("discord.js");
+const client = new Discord.Client();
 
-var bot = new Discord.Client();
+var prefix = ("!");
 
-bot.on("meesage", function(message) {
- console.log(message.content); 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+ if (msg.content.toLowerCase() === prefix + 'ping') {
+    msg.reply('Pong! **' + client.ping + '** ms');
+  }
+ if (msg.content.toLowerCase() === 'sa') {
+    msg.reply('Aleyküm Selam!')
+  }
+  if (msg.content.toLowerCase() === 'merhaba') {
+    msg.reply('Aleyküm Selam!')
+  }
+  if (msg.content.toLowerCase() === 'fotoğrafım ne') {
+    msg.reply(msg.author.avatarURL)
+  }
+  if (msg.content.toLowerCase() === 'selamün aleyküm') {
+    msg.reply('Aleyküm Selam!')
+  }
+  if (msg.content.toLowerCase() === prefix + 'bot') {
+    msg.reply('!ping,!bot')
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
