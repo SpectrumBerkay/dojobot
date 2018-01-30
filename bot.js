6dda28f537e4d62f975bd0e6863b84a998c0c9ce
SpectrumBerkay <35933635+SpectrumBerkay@users.noreply.github.com>
2018-01-30 09:48:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', msg => {
     msg.reply('Aleyküm Selam!')
   }
   if (msg.content.toLowerCase() === 'fotoğrafım ne') {
-    message.reply(message.author.avatarURL)
+    msg.reply(message.author.avatarURL)
   }
 });
 
