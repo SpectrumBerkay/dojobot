1e2435ba54a73fc14e4584d0bdc9dd22ca0623a5
spctrum <35933635+spctrum@users.noreply.github.com>
2020-08-29 00:09:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-var prefix = ("fbebsi");
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -10,26 +8,7 @@ client.on('ready', () => {
 client.on('message', msg => {
  if (msg.content.toLowerCase() === prefix + 'ping') {
     msg.reply('Pong! **' + client.ping + '** ms');
-  }
- if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('Aleyküm Selam!')
-  }
-  if (msg.content.toLowerCase() === 'merhaba') {
-    msg.reply('Aleyküm Selam!')
-  }
-  if (msg.content.toLowerCase() === 'fotoğrafım ne') {
-    msg.reply(msg.author.avatarURL)
-  }
-  if (msg.content.toLowerCase() === 'selamün aleyküm') {
-    msg.reply('Aleyküm Selam!')
-  }
-  if (msg.content.toLowerCase() === 'bot komutları') {
-    msg.channel.sendMessage('fbebsiping,fbebsibot')
-  }
-  if (msg.content.toLowerCase() === 'embedyaz') {
-    msg.channel.sendMessage('embed')
-  }
-  
+  }  
 });
 
 client.login(process.env.BOT_TOKEN);
